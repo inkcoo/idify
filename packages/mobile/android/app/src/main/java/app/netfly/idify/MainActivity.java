aca4e377e39457b81bf877588be49d9d75000d9e
inkcoo <ixiaocang@foxmail.com>
2025-10-21 15:32:50 +0800

    web

--- packages/mobile/android/app/src/main/java/app/netfly/idify/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package app.netfly.idify;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- packages/mobile/src/polyfill.ts
+++
@@ -1,6 +0,0 @@
-if (import.meta.env.DEV) {
-  window.gtag = () => {}
-  import('eruda').then(({ default: eruda }) => {
-    eruda.init()
-  })
-}
